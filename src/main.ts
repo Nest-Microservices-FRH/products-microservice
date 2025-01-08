9926f090982999bf10d3285e3fdef89ef806d733
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-01-09 00:25:49 +0100

    slint fix

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,17 @@ import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
 
-async function bootstrap() {
-  const logger = new Logger('Main');
+async function bootstrap(): Promise<void> {
+    const logger = new Logger('Main');
 
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
-  await app.listen(envs.port);
-  logger.log(`Application is running on port: ${envs.port}`);
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist           : true,
+            forbidNonWhitelisted: true,
+        }),
+    );
+    await app.listen(envs.port);
+    logger.log(`Application is running on port: ${envs.port}`);
 }
 bootstrap();
