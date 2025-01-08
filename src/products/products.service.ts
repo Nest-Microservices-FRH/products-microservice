52c301a74c12b14898eb6fec157a610165b56ddc
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-01-08 15:59:58 +0100

    slint fix

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -5,32 +5,32 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
-  private readonly logger = new Logger(ProductsService.name);
+    private readonly logger = new Logger(ProductsService.name);
 
-  onModuleInit() {
-    this.$connect();
-    this.logger.log('Connected to the database');
-  }
+    onModuleInit() {
+        this.$connect();
+        this.logger.log('Connected to the database');
+    }
 
-  create(createProductDto: CreateProductDto) {
-    return this.product.create({
-      data: createProductDto,
-    });
-  }
+    create(createProductDto: CreateProductDto) {
+        return this.product.create({
+            data: createProductDto,
+        });
+    }
 
-  findAll() {
-    return this.product.findMany({});
-  }
+    findAll() {
+        return this.product.findMany({});
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
-  }
+    findOne(id: number) {
+        return `This action returns a #${id} product`;
+    }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
+    update(id: number, updateProductDto: UpdateProductDto) {
+        return `This action updates a #${id} product`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
-  }
+    remove(id: number) {
+        return `This action removes a #${id} product`;
+    }
 }
