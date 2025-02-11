028d4bbb1f15695e5b26db2e51fba5d94dda554f
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-02-11 11:44:53 +0100

    changes to set nats conecction

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,24 +4,30 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+  NATS_SERVERS: string[];
 }
 
 const envsSchema = joi
-  .object({
-    PORT: joi.number().required(),
-    DATABASE_URL: joi.string().required(),
-  })
-  .unknown(true);
+    .object({
+        PORT        : joi.number().required(),
+        DATABASE_URL: joi.string().required(),
+        NATS_SERVERS: joi.array().items(joi.string()).required(),
+    })
+    .unknown(true);
 
-const { error, value } = envsSchema.validate(process.env);
+const { error, value } = envsSchema.validate({
+    ...process.env,
+    NATS_SERVERS: process.env.NATS_SERVERS?.split(','), // Split the NATS_SERVERS env var into an array
+});
 
 if (error) {
-  throw new Error(`Config validation error: ${error.message}`);
+    throw new Error(`Config validation error: ${error.message}`);
 }
 
 const envVars: EnvVars = value;
 
 export const envs = {
-  port: envVars.PORT,
-  databaseUrl: envVars.DATABASE_URL,
+    port       : envVars.PORT,
+    databaseUrl: envVars.DATABASE_URL,
+    natServers : envVars.NATS_SERVERS,
 };
